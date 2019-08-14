258b11a25d05da2965a31893ef3de42886e35354
skddls88 <51692156+skddls88@users.noreply.github.com>
2019-08-14 15:00:13 +0900

    코레일 자동 예매 매크로를 만들어 보자

---
+++ 코레일메크로/app/user.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return '<h1>Hello world!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
+
+if __name__ == '__main__':
+	app.run(debug=True)
