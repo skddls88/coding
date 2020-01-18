8a878c4350472fa37675251df1e469e33f2d7dcb
skddls88 <51692156+skddls88@users.noreply.github.com>
2020-01-18 18:41:31 +0900

    1
    1

---
+++ minitoy/templates/index.js
@@ -0,0 +1,12 @@
+
+var hwp = new ActiveXObject('HWPFrame.HwpObject.1');
+hwp.HAction.GetDefault('InsertText', hwp.HParameterSet.HInsertText.HSet);	// 텍스트 입력
+hwp.HParameterSet.HInsertText.Text = '테스트입니다.';
+hwp.HAction.Execute('InsertText', hwp.HParameterSet.HInsertText.HSet);
+hwp.HAction.Run('BreakPara');	// 엔터 입력
+hwp.HAction.GetDefault('InsertText', hwp.HParameterSet.HInsertText.HSet);	// 텍스트 입력
+hwp.HParameterSet.HInsertText.Text = '테스트입니다222';
+hwp.HAction.Execute('InsertText', hwp.HParameterSet.HInsertText.HSet);
+hwp.HAction.Run('SelectAll');	// 모두선택
+hwp.HAction.Run('CharShapeBold');	// 진하게
+hwp.HAction.Run('Cancel');	// 블록해제
